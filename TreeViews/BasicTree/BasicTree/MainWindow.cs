ab348d8fac9187f675ce0e5d7e112fe8384d0447
Jason Holland <jholland918@gmail.com>
2013-04-05 23:31:38 -0500

    Add BasicTree TreeView sample.

---
+++ TreeViews/BasicTree/BasicTree/MainWindow.cs
@@ -0,0 +1,76 @@
+using System;
+using Gtk;
+
+/// <summary>
+/// A very basic TreeView with a TreeStore as the model.
+/// </summary>
+public partial class MainWindow : Gtk.Window
+{    
+    private TreeStore treeStore1;
+
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        this.Build();
+        this.SetupTreeView();
+    }
+    
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    private void SetupTreeView()
+    {
+        {
+            var column = new TreeViewColumn();
+            var cell = new CellRendererText();
+            column.Title = "Customer ID";
+            column.PackStart(cell, true);
+            column.AddAttribute(cell, "text", 0);
+            this.treeview1.AppendColumn(column);
+        }
+
+        {
+            var column = new TreeViewColumn();
+            var cell = new CellRendererText();
+            column.Title = "First Name";
+            column.PackStart(cell, true);
+            column.AddAttribute(cell, "text", 1);
+            this.treeview1.AppendColumn(column);
+        }
+
+        {
+            var column = new TreeViewColumn();
+            var cell = new CellRendererText();
+            column.Title = "Last Name";
+            column.PackStart(cell, true);
+            column.AddAttribute(cell, "text", 2);
+            this.treeview1.AppendColumn(column);
+        }
+
+        this.treeStore1 = (TreeStore)this.SetupTreeViewModel();
+        
+        this.treeview1.Model = this.treeStore1;
+        this.treeview1.ExpandAll();
+    }
+    
+    private TreeModel SetupTreeViewModel()
+    {
+        TreeIter iter;
+        var model = new TreeStore(typeof(string), typeof(string), typeof(string));
+        
+        iter = model.AppendValues("Last Name starting with D");
+        model.AppendValues(iter, "4", "Basia", "Dunlap");
+        model.AppendValues(iter, "5", "Madaline", "Durham");
+        
+        iter = model.AppendValues("Last Name starting with N");
+        model.AppendValues(iter, "2", "Ryan", "Nieves");
+        
+        iter = model.AppendValues("Last Name starting with S");
+        model.AppendValues(iter, "1", "Bert", "Sanders");
+        model.AppendValues(iter, "3", "Cruz", "Strong");
+        
+        return model;
+    }
+}
